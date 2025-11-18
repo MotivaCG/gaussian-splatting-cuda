5969e70701c2681498bc9299fff145db3b1962fe
MotivaCG <victor@motivacg.com>
2025-11-18 11:23:09 +0100

    Update projection_fast.hpp

--- src/training/rasterization/projection_fast.hpp
+++ src/training/rasterization/projection_fast.hpp
@@ -1,5 +1,7 @@
 #pragma once
-#include "Projection.h"
+#include "Cameras.h"    // Define UnscentedTransformParameters, ShutterType
+#include "Common.h"     // Define CameraModelType
+#include "Projection.h" // Define launch_projection_ut_3dgs_fused_kernel
 #include <optional>
 #include <torch/torch.h>
 
@@ -59,11 +61,13 @@ namespace gs {
             auto scaled_scales = scales * scaling_modifier;
 
             // Create output tensors
-            auto options = torch::TensorOptions().dtype(means3D.dtype()).device(means3D.device());
-            torch::Tensor radii = torch::empty({C, N, 2}, options);
-            torch::Tensor means2d = torch::empty({C, N, 2}, options);
-            torch::Tensor depths = torch::empty({C, N}, options);
-            torch::Tensor conics = torch::empty({C, N, 3}, options);
+            auto float_options = torch::TensorOptions().dtype(means3D.dtype()).device(means3D.device());
+            auto int_options = torch::TensorOptions().dtype(torch::kInt32).device(means3D.device());
+
+            torch::Tensor radii = torch::empty({C, N, 2}, int_options); // radii debe ser int32
+            torch::Tensor means2d = torch::empty({C, N, 2}, float_options);
+            torch::Tensor depths = torch::empty({C, N}, float_options);
+            torch::Tensor conics = torch::empty({C, N, 3}, float_options);
 
             // Call the projection kernel
             gsplat::launch_projection_ut_3dgs_fused_kernel(
